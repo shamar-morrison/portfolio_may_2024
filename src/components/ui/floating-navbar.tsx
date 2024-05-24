5497c8b2084ce600da9ab165e69b832e626e27c2
Shamar Morrison <shamar.webdev@gmail.com>
2024-05-23 19:11:52 -0500

    feat: Add ShimmerButton component to FloatingNav
    The code changes import the ShimmerButton component and add it to the FloatingNav component. This enhances the user experience by providing a shimmer effect on the "Download CV" button.

--- src/components/ui/floating-navbar.tsx
+++ src/components/ui/floating-navbar.tsx
@@ -8,6 +8,7 @@ import {
 } from 'framer-motion'
 import { cn } from '@/lib/utils'
 import Link from 'next/link'
+import ShimmerButton from '@/components/ui/shimmer-button'
 
 export type NavItem = {
   name: string
@@ -58,7 +59,7 @@ export const FloatingNav = ({
           duration: 0.2,
         }}
         className={cn(
-          'fixed inset-x-0  top-10 z-[5000] mx-auto flex max-w-fit items-center justify-center space-x-4 rounded-full border border-transparent bg-[#ffd6d6] bg-opacity-5 py-2 pl-8 pr-2 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)]  backdrop-blur-lg dark:border-white/[0.2] dark:bg-black',
+          'fixed inset-x-0  top-10 z-[5000] mx-auto flex max-w-fit items-center justify-center space-x-4 rounded-full border border-[#ffd6d6] bg-[#ffd6d6] bg-opacity-5 py-2 pl-8 pr-2 shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_0px_0px_1px_rgba(25,28,33,0.08)]  backdrop-blur-lg dark:border-white/[0.2] dark:bg-black',
           className,
         )}>
         {navItems.map((navItem: any, idx: number) => (
@@ -70,10 +71,11 @@ export const FloatingNav = ({
             <span className='hidden text-sm sm:block'>{navItem.name}</span>
           </Link>
         ))}
-        <button className='relative rounded-full border border-neutral-200 px-4 py-2 text-sm font-medium text-black dark:border-white/[0.2] dark:text-white'>
-          <span>Login</span>
-          <span className='absolute inset-x-0 -bottom-px mx-auto h-px w-1/2 bg-gradient-to-r from-transparent via-blue-500  to-transparent' />
-        </button>
+        <ShimmerButton className='shadow-2xl'>
+          <span className='whitespace-pre-wrap text-center text-sm font-medium leading-none tracking-tight text-white dark:from-white dark:to-slate-900/10 '>
+            Download CV
+          </span>
+        </ShimmerButton>
       </motion.div>
     </AnimatePresence>
   )
